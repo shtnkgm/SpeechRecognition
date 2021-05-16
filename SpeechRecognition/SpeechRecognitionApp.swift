d6f0ad433534e70146b7c98f25ab1a85b1457d55
snakagam <snakagam@yahoo-corp.jp>
2021-05-16 09:38:44 +0900

    Initial Commit

---
+++ SpeechRecognition/SpeechRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechRecognitionApp.swift
+//  SpeechRecognition
+//
+//  Created by snakagam on 2021/05/16.
+//
+
+import SwiftUI
+
+@main
+struct SpeechRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
